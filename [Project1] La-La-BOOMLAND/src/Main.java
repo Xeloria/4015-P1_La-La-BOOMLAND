a45f692a69d04851b688f6370f5b871839d267cf
cintr <cintr@10.32.28.188>
2017-04-06 13:28:44 -0400

    Edit: Instructions;

--- [Project1] La-La-BOOMLAND/src/Main.java
+++ [Project1] La-La-BOOMLAND/src/Main.java
@@ -12,30 +12,6 @@ public class Main {
 		myFrame.setSize(350, 410);
 		
 		
-		String welcome = "Welcome to La-La-BOOMLAND!";
-		String dash = "\n-----------------------------";
-		String instructions = "\nClick inside the grid to clear it.\nBe careful not to hit a mine!\nFlag a square you suspect has a mine\nwith the mouse right button.\nFlag all 10 mines to win!";
-		String colorCode = "\nColor code for nearby mines:\nNo mines: Light Cyan\n1 mine: Blue\n2 mines: Light Puerple\n3 mines: Purple\n4 mines: pink\n5 mines: Orange\n6 mines: Yellow\n7 mines: Lime\n8 mines: Dark Magenta";
-		
-		
-		
-//		JLabel legend = new JLabel(welcome+"\n"+dash+"\n"+instructions+"\n"+dash+"\n"+colorCode);
-//		legend.setBackground(Color.BLACK);
-//		legend.setForeground(Color.WHITE);
-////		legend.setLocation(400, 150);
-//		JFrame instructionFrame = new JFrame("Instructions");
-//		instructionFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//		instructionFrame.setLocation(500, 150);
-//		instructionFrame.setSize(350, 410);
-//		instructionFrame.add(legend);
-//		instructionFrame.setVisible(true);
-		
-		
-		
-		
-		
-		
-		
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
 		
@@ -54,16 +30,19 @@ public class Main {
 		+ "Flag a square you suspect has a " + "<br>" 
 		+ "mine with the mouse right button." + "<br>" 
 		+ "Flag all 10 mines to win!" + "<br>"
+		+ "When you lose the top button turns RED." + "<br>"
+		+ "and BLUE when you win." + "<br>"
+		+ "Click the top button to begin a new game!" + "<br>"
 		+ "----------------------------------" + "<br>"
-		+ "No mines = Light Cyan" + "<br>"
-		+ "1 mine = Blue" + "<br>"
-		+ "2 mines = Indigo" + "<br>"
-		+ "3 mines = Yellow" + "<br>"
-		+ "4 mines = Pink" + "<br>"
-		+ "5 mines = Orange" + "<br>"
-		+ "6 mines = Purple" + "<br>"
-		+ "7 mines = Lime" + "<br>"
-		+ "8 mines = Dark Magenta" + "<br>";
+		+ "No mines = Light CYAN" + "<br>"
+		+ "1 mine = BLUE" + "<br>"
+		+ "2 mines = INDIGO" + "<br>"
+		+ "3 mines = YELLOW" + "<br>"
+		+ "4 mines = PINK" + "<br>"
+		+ "5 mines = ORANGE" + "<br>"
+		+ "6 mines = PURPLE" + "<br>"
+		+ "7 mines = LIME" + "<br>"
+		+ "8 mines = DARK MAGENTA" + "<br>";
 	
 		JLabel Legend = new JLabel ("<html><div style='text-align: center;'>" + Title + "</div></html>");
 		
@@ -71,7 +50,7 @@ public class Main {
 		JFrame myLegend = new JFrame("La-La-Legend");
 		myLegend.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myLegend.setLocation(800, 150);
-		myLegend.setSize(200, 400);
+		myLegend.setSize(250, 450);
 		myLegend.add(Legend);
 		myLegend.setVisible(true);
 	
